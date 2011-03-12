c1df9449ddb7e28fa03dff5285b166ad6debeead
Mauro <mrsanna1@gmail.com>
2011-03-12 20:56:11 +0100

    dashboard index view

--- features/step_definitions/start_application_steps.rb
+++ features/step_definitions/start_application_steps.rb
@@ -13,3 +13,13 @@ end
 Then /^I should redirect to sign_in page$/ do                                                             
     visit  "/users/sign_in"                                       
 end 
+
+Given /^I am an authenticated user$/ do                                                                   
+    email = 'mrsanna1@gmail.com'                                       
+    login = 'mauro.sanna'
+    password = 'secret'
+end                                                                                                       
+
+When /^I access the root of application$/ do                                                              
+    visit '/'                                       
+end 
