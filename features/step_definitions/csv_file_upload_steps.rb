7fde487c7e284603a05292b4179743e57807e403
Mauro <mrsanna1@gmail.com>
2011-03-12 22:36:27 +0100

    csv_file_upload_steps.rb

---
+++ features/step_definitions/csv_file_upload_steps.rb
@@ -0,0 +1,23 @@
+Given /^I am an admin user$/ do                                                                           
+    pending # express the regexp above with the code you wish you had                                       
+end                                                                                                       
+                                                                                                          
+When /^I visit the csv import page$/ do                                                                   
+    pending # express the regexp above with the code you wish you had                                       
+end                                                                                                       
+                                                                                                          
+When /^I upload a file with valid data for the customers$/ do                                             
+    pending # express the regexp above with the code you wish you had                                       
+end                                                                                                       
+                                                                                                          
+Then /^I should be sent to the csv uploaded page$/ do                                                     
+    pending # express the regexp above with the code you wish you had                                       
+end                                                                                                       
+                                                                                                          
+When /^I upload a malformed file$/ do                                                                     
+    pending # express the regexp above with the code you wish you had                                       
+end                                                                                                       
+                                                                                                          
+Then /^I should be sent to (\d+)\.html page$/ do |arg1|                                                   
+    pending # express the regexp above with the code you wish you had                                       
+end 
