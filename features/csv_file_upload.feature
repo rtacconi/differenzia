1d348c8eb3c6c7050362dc6fdc193180ae886069
Mauro <mrsanna1@gmail.com>
2011-03-13 01:18:13 +0100

    csv_file_upload_steps.rb

--- features/csv_file_upload.feature
+++ features/csv_file_upload.feature
@@ -4,14 +4,14 @@ Feature: CSV file upload
   I want to be able to upload a csv file with customers data
 
   Scenario: Uploading a valid file with data for the customers
-    Given I am an admin user
+    Given I am an user with role "ADMIN"
     When I visit the csv import page
     And I upload a file with valid data for the customers
     Then I should be sent to the csv uploaded page
     And I should see "Uploaded successfully!"
 
   Scenario: Uploading a malformed file
-    Given I am an admin user
+    Given I am an user with role "ADMIN"
     When I visit the csv import page
     And I upload a malformed file
     Then I should be sent to 500.html page
--- features/step_definitions/csv_file_upload_steps.rb
+++ features/step_definitions/csv_file_upload_steps.rb
@@ -1,25 +1,30 @@
-Given /^I am an admin user$/ do                                                                           
-    pending # express the regexp above with the code you wish you had                                       
-end                                                                                                       
-                                                                                                          
+Given /^I am an user with role "ADMIN"$/ do                                                             
+  email = "mrsanna1@gmail.com"                         
+  login = "mauro.sanna"
+  password = "secret"
+  role = "ADMIN"
+  #User.findByRole("ADMIN")
+end
+
 When /^I visit the csv import page$/ do                                                                   
-    pending # express the regexp above with the code you wish you had                                       
+  visit "/csv/import"                                       
 end                                                                                                       
                                                                                                           
-When /^I upload a file with valid data for the customers$/ do                                             
-    attach_file(:csv, File.join(RAILS_ROOT, 'public', 'data', 'csv_ok'))                                 
-    click_button "Upload"
+When /^I upload a file with valid data for the customers$/ do                                       
+  attach_file :upload_csv, File.join(Rails.root.to_s, 'public', 'data', 'csv_ok')               
+  click_button "Upload"
 end                                                                                                       
                                                                                                           
 Then /^I should be sent to the csv uploaded page$/ do                                                     
-    pending # express the regexp above with the code you wish you had                                       
+  pending # express the regexp above with the code you wish you had
 end                                                                                                       
                                                                                                           
 When /^I upload a malformed file$/ do                                                                     
-    attach_file(:csv, File.join(RAILS_ROOT, 'public', 'data', 'csv_ok')) 
-        click_button "Upload"                                     
+  attach_file :upload_csv, File.join(Rails.root.to_s, 'public', 'data', 'csv_not_ok') 
+  click_button "Upload"                                     
 end                                                                                                       
                                                                                                           
 Then /^I should be sent to (\d+)\.html page$/ do |arg1|                                                   
-    pending # express the regexp above with the code you wish you had                                       
+  pending # express the regexp above with the code you wish you had                                   
 end 
+
