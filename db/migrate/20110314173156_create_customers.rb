efeca8402e5fb8ca487c7df5e1a1a04ab8661a26
Mauro <mrsanna1@gmail.com>
2011-03-14 21:30:45 +0100

    modificato customers migration

--- db/migrate/20110314173156_create_customers.rb
+++ db/migrate/20110314173156_create_customers.rb
@@ -1,12 +1,17 @@
 class CreateCustomers < ActiveRecord::Migration
   def self.up
     create_table :customers do |t|
+      t.integer :year
       t.string :full_name
-      t.string :address_line_1
-      t.string :address_line_2
-      t.string :address_line_3
-      t.string :postal_code
-      t.string :country
+      t.date :birth_date
+      t.string :address
+      t.string :postal_code, :limit => 5 #codice postale
+      t.string :locality
+      t.string :tax_code, :limit => 16 #codice fiscale
+      t.string :contract_number
+      t.integer :square_meters
+      t.integer :category
+      t.string :taxable_address #indirizzo per il quale viene fatta la rischiesta
 
       t.timestamps
     end
