0537dc87896580a168a25b0dcce9876a7c131f84
Mauro <mrsanna1@gmail.com>
2011-03-15 10:00:14 +0100

    private method admin in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :admin?
+  
+  private
+  
+  def admin
+    @current_user.role == 'admin'
+  end
+  
 end
