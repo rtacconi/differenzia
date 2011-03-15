78d3b1bdce70c16e6f95c8e8cc6a7ae55d09f631
Riccardo Tacconi <rtacconi@gmail.com>
2011-03-15 21:42:30 +0100

    login for users feature

--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,12 @@
 ActiveRecord::Schema.define(:version => 20110315111001) do
 
   create_table "customers", :force => true do |t|
-    t.integer  "year"
     t.string   "full_name"
-    t.date     "birth_date"
-    t.string   "address"
-    t.string   "postal_code",     :limit => 5
-    t.string   "locality"
-    t.string   "tax_code",        :limit => 16
-    t.string   "contract_number"
-    t.integer  "square_meters"
-    t.integer  "category"
-    t.string   "taxable_address"
+    t.string   "address_line_1"
+    t.string   "address_line_2"
+    t.string   "address_line_3"
+    t.string   "postal_code"
+    t.string   "country"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- features/user_creation.feature
+++ features/user_creation.feature
@@ -1,7 +1,10 @@
 Feature: User creation
 
   Scenario: Create user
-    Given I am a valid user with role "admin"
-    When I go to the root page
+    Given I have an admin user with email address "admin@differenzia.com" 
+    When I go to the sign in page
+    And I fill in "user_email" with "admin@differenzia.com"
+    And I fill in "user_password" with "password"
+    And I press "Sign in"
     And I follow "Operators"
     Then I should see the list of users
