c2e21d642751cca6d5d4de062095577ac336949a
Mauro <mrsanna1@gmail.com>
2011-03-17 21:42:06 +0100

    users views

--- features/step_definitions/user_creation_steps.rb
+++
@@ -1,7 +0,0 @@
-Given /^I am an user with role "([^"]*)"$/ do |role|
-  User.find_by_role("#{role}")                
-end                                                                                    
-
-Then /^I should see the list of users$/ do                                             
-    users_path                    
-end
