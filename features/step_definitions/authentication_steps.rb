cdd5abe0d4cdfed9ae933448d3ff102fdb751256
Riccardo Tacconi <rtacconi@gmail.com>
2011-03-18 00:11:30 +0100

    create user feature improved

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -8,12 +8,12 @@ And /^I should see the link "([^"]*)"$/ do |link|
 end
 
 Given /^I have one user with email "([^"]*)" role "([^"]*)" and password "([^"]*)"$/ do |email, role, password|
-user = User.find_by_email(email)
-if user.present?
-  user
-else
-  User.create!(:role => role, :email => email,
-               :password => password, :password_confirmation => password,
-               :first_name => "John", :last_name => "Doe")
-end
+  user = User.find_by_email(email)
+  if user.present?
+    user
+  else
+    User.create!(:role => role, :email => email,
+                 :password => password, :password_confirmation => password,
+                 :first_name => "John", :last_name => "Doe")
+  end
 end
