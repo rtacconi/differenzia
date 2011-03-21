2aeee95d7ea4f23af6ef2c646a4ce8651834d463
Mauro <mrsanna1@gmail.com>
2011-03-22 00:31:38 +0100

    dashboard_controller spec passed

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -3,8 +3,6 @@ module ControllerMacros
     before(:each) do
     	@user = mock_model(User, :update_attributes => true)
     	User.stub!(:find).and_return(@user)
-      #@user = mock_model(User, :first_name => 'name', :last_name => 'surname', :email => 'user@differenzia.com',
-      #									:role => 'user', :password => 'password', :password_confirmation => 'password')
       @request.env["devise.mapping"] = Devise.mappings[@user]
       sign_in @user
     end
