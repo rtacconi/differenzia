cc85a4858eb0ce775acad77849ec1b2e28d89b99
Mauro <mrsanna1@gmail.com>
2011-03-22 00:29:52 +0100

    dashboard_controller spec passed

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,8 +1,11 @@
 module ControllerMacros
   def login_user
     before(:each) do
-      build :user # Using blueprints gem for fixtures data
-      @request.env["devise.mapping"] = Devise.mappings[:user]
+    	@user = mock_model(User, :update_attributes => true)
+    	User.stub!(:find).and_return(@user)
+      #@user = mock_model(User, :first_name => 'name', :last_name => 'surname', :email => 'user@differenzia.com',
+      #									:role => 'user', :password => 'password', :password_confirmation => 'password')
+      @request.env["devise.mapping"] = Devise.mappings[@user]
       sign_in @user
     end
   end
