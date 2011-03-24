3396fbc40ce4dfcb21a653ee96e2bb5b6dbc4c83
Mauro <mrsanna1@gmail.com>
2011-03-24 10:11:57 +0100

    annotated customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -14,6 +14,8 @@ describe Customer do
   end
 end
 
+
+
 # == Schema Information
 #
 # Table name: customers
@@ -23,8 +25,10 @@ end
 #  full_name       :string(255)
 #  birth_date      :date
 #  address         :string(255)
+#  number          :string(255)
 #  postal_code     :string(5)
-#  locality        :string(255)
+#  city            :string(255)
+#  prov            :string(255)
 #  tax_code        :string(16)
 #  contract_number :string(255)
 #  square_meters   :integer
