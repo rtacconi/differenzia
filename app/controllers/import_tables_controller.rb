71e7b618e70c3023e52d262a1b310c860299a89f
Mauro <mrsanna1@gmail.com>
2011-03-25 11:58:42 +0100

    csv import

--- app/controllers/import_tables_controller.rb
+++ app/controllers/import_tables_controller.rb
@@ -17,7 +17,8 @@ class ImportTablesController < ApplicationController
   	@import_cells = @import_table.import_cells
     @row_index_max = @import_cells.map { |cell| cell.row_index }.max
     @column_index_max = @import_cells.map { |cell| cell.column_index }.max
-    @tables = ActiveRecord::Base.connection.tables.select { |t| t != 'schema_migrations' }
+    # mi serve solo la tabella customers
+    @tables = ActiveRecord::Base.connection.tables.select { |t| t == 'customers' }
 
     respond_to do |format|
       format.html # show.html.erb
