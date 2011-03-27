709267678ef28be232dc363182022134588ef036
Mauro <mrsanna1@gmail.com>
2011-03-28 00:11:42 +0200

    added logger to csv controller

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -5,10 +5,13 @@ class CsvController < ApplicationController
   
   def upload
   	table = ImportTable.new :original_path => params[:upload][:csv].original_filename
+  	row_count = 0 # per sapere il numero di righe
     row_index = 0
     uploaded_file = params[:upload][:csv].read
     FasterCSV.parse(uploaded_file) do |cells|
+    	row_count += 1
     	logger.debug cells
+    	logger.debug row_count # a quale riga è arrivato
       column_index = 0
       cells.each do |cell|
         table.import_cells.build :column_index => column_index, :row_index => row_index, :contents => cell
