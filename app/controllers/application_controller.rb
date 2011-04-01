237844b6df7189702c30e66d584d2e5e4cd01591
Riccardo Tacconi <rtacconi@gmail.com>
2011-04-01 02:19:21 +0200

    compass and app style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :admin?
+  layout :specify_layout
   
   private
+  def specify_layout
+    devise_controller? ? "sign" : "application"
+  end
   
   def admin?
     @current_user.role == 'admin'
