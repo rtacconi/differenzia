a3bca480be5c69b4cbb61aaae4671c77853c6255
Mauro <mrsanna1@gmail.com>
2011-04-07 23:26:34 +0200

    root customers#index

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ Differenzia::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "dashboard#index"
+  root :to => "customers#index"
 
   # See how all your routes lay out with "rake routes"
 
