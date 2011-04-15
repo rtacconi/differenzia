84a0da4c6eef3c898c1f99d4f48ed0babcec7522
Mauro <mrsanna1@gmail.com>
2011-04-16 00:03:31 +0200

    spec customer model pass

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,16 @@
+Spec.configure do |config|
+  
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
