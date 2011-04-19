1956c3c66b3b3021a6d6be62bbe637a18c0b9e41
Mauro <mrsanna1@gmail.com>
2011-04-19 09:43:29 +0200

    ..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,5 @@
-class UsersController < ApplicationController
-	#before_filter :authenticate_user!
-	respond_to :html
+class UsersController < InheritedResources::Base
 	
-  def index
-  	@users = User.all(:order => "created_at DESC")
-  end
-
-  def show
-  	# respond_with(@user = User.find(params[:id]))
-  end
-
-  def new
-  	@user = User.new
-  end
-
-  def edit
-  	@user = User.find(params[:id])
-  end
-
-  def create
-  	@user = User.new(params[:user])
-    flash[:notice] = "Operatore creato con successo." if@user.save
-    respond_with(@user, :location => users_url)
-  end
-
-  def update
-  	@user = User.find(params[:id])
-    flash[:notice] = "Operatore modificato con successo." if @user.update_attributes(params[:user])
-    respond_with(@user, :location => users_url)
-  end
-
   def destroy
     begin
       @user = User.find(params[:id])
