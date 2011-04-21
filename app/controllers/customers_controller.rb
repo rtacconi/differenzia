9546f5275563a09acabc3f44590d931dbfbe30b3
Riccardo Tacconi <rtacconi@gmail.com>
2011-04-21 12:12:12 +0200

    fixed a spec in controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,9 +3,9 @@ class CustomersController < InheritedResources::Base
 	def index
 		@mq_accepted = 12 # valore al di sotto del quale non devono essere consegnati i sacchetti
 		unless params[:customer_full_name].blank?
-		full_name = params[:customer_full_name]
-		@customers = Customer.search_full_name(full_name).paginate(:per_page => 10,
-		  :page => params[:page])
+  		full_name = params[:customer_full_name]
+  		@customers = Customer.search_full_name(full_name).paginate(:per_page => 10,
+  		  :page => params[:page])
 		else
 			@customers = Customer.all.paginate(:per_page => 10, :page => params[:page])
 		end
