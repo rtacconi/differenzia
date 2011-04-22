93aca213f8d2939934e0d98f12e77d446becf5ea
Mauro <mrsanna1@gmail.com>
2011-04-22 22:53:13 +0200

    spec customers controller pass

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,70 +1,44 @@
 require 'spec_helper'
 
-#describe CustomersController do
+describe CustomersController do
 
-  describe "search with js", :js => true  do
+  describe "customer search" do
     before(:each) do
-      User.make!
-      Customer.make!
-      #@customers = mock_model(Customer, :full_name => "User-1").as_null_object
+      @customers = mock_model(Customer, :full_name => "User-1").as_null_object
     end
 
-    context "when full_name is 'user'" do
+    context "when customer_full_name is 'user'" do
       it "should find customers" do
-        #Customer.make!
-        login
-        page.should have_content "Customers"
-        page.should have_content "Dashboard"
-        click_link('Customers')
-        page.should have_content "Nominativo"
-        fill_in "customer_full_name", :with => "User"
-        page.should have_content "Data di nascita"
-        #do_get
-        #@full_name = "User"
-        #Customer.should_receive(:search_full_name).with(@full_name).and_return(@customers)
+        @full_name = "User" 
+        Customer.should_receive(:search_full_name).with(@full_name).and_return(@customers)
         #@customers.should_receive(:paginate)
-        #do_get
-        #assigns(:customers).should_not be_nil
+        do_get
+        assigns(:customers).should_not be_nil
       end
     end
 
-    context "when full_name is blank" do
-      it "should not return any object" do
+    context "when customer_full_name is blank" do
+      it "should return all customers" do
         @full_name = ""
         Customer.should_receive(:all).and_return(@customers)
-        @customers.should_receive(:paginate)
- #       do_get
-        assigns(:customers).should_not be_empty
+        #@customers.should_receive(:paginate)
+        do_get
+        assigns(:customers).should_not be_nil
       end
     end
 
-    context "when full_name is wrong" do
+    context "when customer_full_name not found" do
       it "customers should be nil" do
       	@full_name = "wrong"
         assigns(:customers).should be_nil
-  #      do_get
+        do_get
       end
     end
   end
 
-  def login
-    #User.make!
-    visit '/d/users/sign_in/'
-    fill_in "user_email", :with => "user0001@differenzia.com"
-    fill_in "user_password", :with => "password"
-    click_button "Sign in"
-  end
-
-  def do_get page= nil, format = 'html'
-    #login_user
-    #get 'index', :customer_full_name => @full_name, :format => format
-    page.execute_script("$('#customer_full_name').searchbox({
-        url: '/customers/search',
-        param: 'customer_full_name',
-        dom_id: '#results',
-        delay: 250,
-        loading_css: '#spinner'
-        })")
+  def do_get format = 'html'
+    login_user
+    get 'search', :customer_full_name => @full_name, :format => format
   end
   
-#end
+end
