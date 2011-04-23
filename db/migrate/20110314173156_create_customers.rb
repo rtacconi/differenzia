6d7e5b748d570061f1cbd70dd422cd821291b727
Riccardo Tacconi <rtacconi@gmail.com>
2011-04-23 19:37:20 +0200

    drop import tables and cells

--- db/migrate/20110314173156_create_customers.rb
+++ db/migrate/20110314173156_create_customers.rb
@@ -6,14 +6,14 @@ class CreateCustomers < ActiveRecord::Migration
       t.date :birth_date
       t.string :address
       t.string :number
-      t.string :postal_code, :limit => 5	#codice postale
+      t.string :postal_code, :limit => 5
       t.string :city
       t.string :prov
-      t.string :tax_code, :limit => 16	#codice fiscale o partita iva
+      t.string :tax_code, :limit => 16
       t.string :contract_number
       t.integer :square_meters
       t.integer :category
-      t.string :taxable_address	#indirizzo per il quale viene fatta la rischiesta
+      t.string :taxable_address	# main address
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110324154425) do
+ActiveRecord::Schema.define(:version => 20110423173629) do
 
   create_table "customers", :force => true do |t|
-    t.integer  "year",            :limit => 10
+    t.integer  "year"
     t.string   "full_name"
     t.date     "birth_date"
     t.string   "address"
@@ -23,26 +23,9 @@ ActiveRecord::Schema.define(:version => 20110324154425) do
     t.string   "prov"
     t.string   "tax_code",        :limit => 16
     t.string   "contract_number"
-    t.integer  "square_meters",   :limit => 10
-    t.integer  "category",        :limit => 10
+    t.integer  "square_meters"
+    t.integer  "category"
     t.string   "taxable_address"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "import_cells", :force => true do |t|
-    t.integer  "import_table_id", :limit => 10
-    t.integer  "row_index",       :limit => 10
-    t.integer  "column_index",    :limit => 10
-    t.string   "contents"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "import_cells", ["import_table_id"], :name => "index_import_cells_on_import_table_id"
-
-  create_table "import_tables", :force => true do |t|
-    t.string   "original_path"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -53,7 +36,7 @@ ActiveRecord::Schema.define(:version => 20110324154425) do
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",        :limit => 10,  :default => 0
+    t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
