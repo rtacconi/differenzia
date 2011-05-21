3a77cbdfdea96f40269f5c3086b8de526519fe3c
Mauro <mrsanna1@gmail.com>
2011-05-22 00:26:29 +0200

    ..

--- public/javascripts/delivery_form.js
+++ public/javascripts/delivery_form.js
@@ -1,12 +1,12 @@
 $(function() {
   $('input[name=select_delivery_type]:radio').change(function(){
 	if ($("input[@name='select_delivery_type']:checked").val() == 'default_delivery') {
-	  $('#product_selection').fadeOut('slow');
-      $('#standard_delivery').fadeIn('slow');
+	  $('#product_selection').fadeOut(2000);
+      $('#standard_delivery').fadeIn(2000);
     }  
     else {
-      $('#standard_delivery').fadeOut('slow');
-      $('#product_selection').fadeIn('slow'); 
+      $('#standard_delivery').fadeOut(2000);
+      $('#product_selection').fadeIn(2000); 
     }
   });
 })
