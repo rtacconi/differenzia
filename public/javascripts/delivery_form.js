a32278016058cb40e5a429dafb65c78aedd6b754
Mauro <mrsanna1@gmail.com>
2011-08-11 10:40:10 +0200

    ..

--- public/javascripts/delivery_form.js
+++ public/javascripts/delivery_form.js
@@ -19,10 +19,8 @@ function remove_fields(link) {
     $(link).closest(".fields").hide();  
 }
 
-function add_fields(link, association, content) {  
-	var new_id = new Date().getTime();  
-	var regexp = new RegExp("new_" + association, "g");  
-	$(link).up().insert({
-		before: content.replace(regexp, new_id)  
-	});  
+function add_fields(link, association, content) {
+	var new_id = new Date().getTime();
+	var regexp = new RegExp("new_" + association, "g");
+	$(link).parent().before(content.replace(regexp, new_id));
 }  
